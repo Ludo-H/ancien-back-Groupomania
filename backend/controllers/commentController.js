a0ed7929fc2a58276791d9bdbac1e6c5a69da63f
Ludo Hund <Ludovic_hund@hotmail.fr>
2022-06-29 09:53:53 +0200

    ajout controller comment

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,6 +1,15 @@
+// On pourra utiliser directement database.query(...)
+const database = require("../database/database").getDatabase();
+
 exports.getAllComment = async (req, res)=>{
     try {
-
+        // On lance la requete
+        const sql = "SELECT * FROM comments";
+        database.query(sql, (error, result)=>{
+            if(error) throw error;
+            // Affichage des infos des comm, a voir si on sélectionne les resultats...
+            res.status(200).json(result);
+        });
     } catch (error) {
         res.status(400).json("Erreur affichage des comments " + error);
     };
@@ -8,7 +17,14 @@ exports.getAllComment = async (req, res)=>{
 
 exports.getOneComment = async (req, res)=>{
     try {
-
+        // On isole la requete
+        const sql = "SELECT * FROM comments WHERE comment_id = ?";
+        // On stocke l'id voulu de la requete
+        const commentId = req.params.id;
+        database.query(sql, commentId, (error, result)=>{
+            if(error) throw error;
+            res.status(200).json(result);
+        });
     } catch (error) {
         res.status(400).json("Erreur affichage du comment " + error);
     };
@@ -16,7 +32,18 @@ exports.getOneComment = async (req, res)=>{
 
 exports.createComment = async (req, res)=>{
     try {
-
+        // Création du contenu à envoyer
+        const newComment = {
+            comment_text: req.body.text,
+            user_id: req.body.userId,
+            post_id: req.body.postId
+        };
+        // Envoie dans la BDD
+        const sql = "INSERT INTO comments SET ?";
+        database.query(sql, newComment, (error, result)=>{
+        if(error) res.status(400).json(error);
+        res.status(200).json("Commentaire créé !");
+    }); 
     } catch (error) {
         res.status(400).json("Erreur création comment " + error);
     };
@@ -32,7 +59,12 @@ exports.modifyComment = async (req, res)=>{
 
 exports.deleteComment = async (req, res)=>{
     try {
-
+        const sql = "DELETE FROM comments WHERE comment_id = ?";
+        const commentId = req.params.id;
+        database.query(sql, commentId, (error, result)=>{
+            if(!result) throw error;
+            res.status(200).json("Comment supprimé");
+        });
     } catch (error) {
         res.status(400).json("Erreur suppression comment " + error);
     };
